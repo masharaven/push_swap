962ff88ac6731b895dba261043fdce67bde23c2b
masharaven <74605798+masharaven@users.noreply.github.com>
2021-07-18 16:34:03 +0300

    Add files via upload

---
+++ push_swap/push_swap.h
@@ -0,0 +1,9 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+#include <unistd.h>
+#include <stdio.h>
+
+typedef struct s_struct
+{
+
+}   t_struct;
---
+++ push_swap/sorting.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+char *merge(char *a)
+{
+	while (ft_strlen(a) > 1)
+	
+}
+
+char	t_sorted(char *a)
+{
+	int	i;
+	int	j;
+	int	res[];
+
+	while (i < ft_strlen(a) && j < ft_strlen())
+}
+
+int main(int argc, char **argv)
+{
+	int i = argc; // количество элементов
+	//int a[] = {2,6,9,1,7,4};
+	int	*a = argv;
+	printf("%s", ft_sorted(a[i]));
+	return 0;
+}
